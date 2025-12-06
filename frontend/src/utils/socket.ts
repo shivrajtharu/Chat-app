16c67339f7d89a22be2576d0a6de9a536c4ced98
shivrajtharu <shivrajtharu62@gmail.com>
2025-12-06 11:51:14 +0545

    added vercel domain

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const createSocket = (token: string) =>
-  io("http://localhost:8000", {
+  io(import.meta.env.VITE_API_URL, {
     auth: { token },
     transports: ["websocket", "polling"],
   });
